4a03cadf5aed5acae3b27ac8a08b5ef74e970706
hataken <bb52119638@gmail.com>
2025-03-03 16:28:54 +0900

    ヘッダー修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,21 @@
     @vite('resources/css/app.css')
   </head>
   <header>
+    <!--PC用のヘッダー-->
     <div class="w-[90vw] mx-auto py-[10px] flex items-center bg-white h-[80px] relative transition-all duration-100">
       <a href="{{ route('top') }}">
-        <img src="{{ asset('images/Herigo_logo.png') }}" alt="サイトのロゴ" style="width: 80px; height: 60px;">
+        <img src="{{ asset('images/Herigo_logo.png') }}" alt="サイトのロゴ" class="w-[80px] h-[60px]">
       </a>
       <a href="{{ route('top') }}" class="font-bold pl-4 text-5xl">Herigo</a>
+      <div class="flex items-center fixed top-[10px] left-1/2 transform -translate-x-1/2 mt-[10px]">
+        <form action="" method="GET" class="relative">
+          <input type="text" name="query" placeholder="世界遺産名で検索" class="w-[25vw] p-[10px_20px_10px_40px] border border-[0.5px] border-gray-400 shadow-md shadow-black/20 rounded-[30px] cursor-pointer bg-white hover:shadow-black/50">
+          <img src="{{ asset('images/search.svg') }}" alt="検索" class="absolute top-[10px] left-[10px] w-[25px] h-[25px]">
+        </form>
+      </div>
     </div>
+    <!--スマホ用のヘッダー-->
+    
   </header>
   <body>
     @yield('content')
