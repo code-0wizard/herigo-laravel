7d795d15f612a62d9716da1239a95341bd4c2556
hataken <bb52119638@gmail.com>
2025-03-07 17:15:54 +0900

    環境構築完了

---
+++ src/resources/js/Pages/SamplePage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container mt-5">
+        <h1 class="text-primary">{{ message }}</h1>
+        <button class="btn btn-success" @click="checkType">Click Me</button>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+    name: 'SamplePage',
+    props: {
+        message: {
+            type: String as PropType<string>,
+            required: true,
+        },
+    },
+    methods: {
+        checkType() {
+            const value: number = 1;
+            console.log(value);
+            const unusedVar = 'This is not used';
+            console.log(unusedVar);
+        },
+    },
+});
+</script>
+<style scoped>
+
+</style>
---
+++ src/routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SampleController;
+
+Route::get('/sample', [SampleController::class, 'sample']);
